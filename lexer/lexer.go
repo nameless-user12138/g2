68e55d3256c107b6d4a10bb76ebc42b6220f6a73
fanghongji <kavergp@gmail.com>
2025-08-30 18:53:23 +0800

    add move math operator support in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -51,6 +51,12 @@ func (l *Lexer) NextToken() token.Token {
 		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.CLASH, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 	case ';':
